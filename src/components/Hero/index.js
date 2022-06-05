09da1918b19be47e0e9fd2cbd501727dda8007bc
brunosilvainteli <bruno.silva@inteli.edu.br>
2022-06-04 22:30:44 -0300

    Update Carrer and Curriculum.

--- src/components/Hero/index.js
+++ src/components/Hero/index.js
@@ -8,7 +8,7 @@ export default function Hero() {
       <p className="text-1">Olá, meu nome é</p>
       <h1 className="text-2">Bruno Silva</h1>
       <h3 className="text-3">Dev Front-End, UX/UI Designer e Designer Gráfico.</h3>
-      <a href="https://drive.google.com/file/d/1IJf5GRmhxALyQwj6_hpmmO8KxcCNA2Dz/view?usp=sharing" target="_blank" rel="noreferrer">
+      <a href="https://drive.google.com/file/d/1_O4BhfrrrojUAei7h_MliVsLh0dGuODp/view?usp=sharing" target="_blank" rel="noreferrer">
         <span className="botao-likedin">Meu Currículo</span>
       </a>
     </div>
